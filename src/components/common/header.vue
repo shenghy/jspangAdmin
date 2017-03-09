fa67c86b3814de3a4ad346449b1cb30307f28335
““ShengHongYu <web0432@126.com>
2017-03-09 15:51:59 +0800

    头部区域完成提交

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -29,42 +29,135 @@
 
            <el-col :xs="8" :sm="8" :md="{span:2,offset: 4}" >
                 <div class="header-right">
-                    <span>
-                        <el-badge :value="5" class="item">
-                            <i class="el-icon-message"></i>
-                        </el-badge>
-                    </span>
+
+                    <!--这是消息的下拉列表 用了element里的 dropdown组件-->                
+                    <el-dropdown menu-align="start" >
+                        <span>
+                            <el-badge :value="5" class="item">
+                                <i class="el-icon-message"></i>
+                            </el-badge>
+                        </span>
+                         <el-dropdown-menu slot="dropdown" >
+                            <el-dropdown-item >
+                               <span class="pop-title">You have new messages! </span>
+                            </el-dropdown-item>
+
+                            <el-dropdown-item  v-for="dialog in dialogs" :key="dialog.time" divided>
+                                <div class="pop-div">
+                                    <span class="pop-image">
+                                        <img :src="dialog.header" width="50px" >
+                                    </span>
+                                    <span class="pop-content">
+                                       {{dialog.content}}
+                                    </span>
+                                    <span class="pop-time">
+                                        {{dialog.time}}分钟前
+                                    </span>
+                                </div>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
 
 
-                    <span>
-                     <el-badge :value="3" class="item">
-                        <i class="el-icon-date"></i>
-                     </el-badge>
-                    </span>
+                    <el-dropdown menu-align="start"  >
+                        <span >
+                            <el-badge :value="3" class="item">
+                                <i class="el-icon-date"></i>
+                            </el-badge>
+                        </span>
+                        <el-dropdown-menu slot="dropdown" >
+                            <el-dropdown-item >
+                               <span class="pop-title">You have new tasks! </span>
+                            </el-dropdown-item>
+                            <el-dropdown-item v-for="task in tasks" :key="task.id" divided>
+                                <div class="task-div">
+                                    <span class="task-span task-tag">
+                                    <el-tag type="danger" v-if="task.rank==1">紧急</el-tag>
+                                    <el-tag type="warning" v-if="task.rank==2">重要</el-tag>
+                                    <el-tag type="success" v-if="task.rank==3">正常</el-tag>
+                                    </span>
+                                    <span class="task-span task-content">
+                                    {{task.content}}<br/>
+                                    <span class="task-time">截止：{{task.overTime}}</span>
+                                    </span>
+                                    
+                                    <span class="task-span task-btn"><el-button type="primary" size="mini">完成</el-button></span>
+                                </div>
+                            </el-dropdown-item>
+
+                        </el-dropdown-menu>
+                    </el-dropdown>
+
+
                 </div>
             
          </el-col>
 
          <el-col :xs="8" :sm="8" :md="2" >
             <div class="user-header">
-                <img src="../../../static/images/b_header.jpg" width="50px" />
+                <el-dropdown trigger="click" menu-align="start">
+                    <img src="../../../static/images/b_header.jpg" width="50px" />
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item >
+                            <div class="setting-div">
+                                <span class="setting-icon"><i class="material-icons">account_box</i></span> 
+                                <span class="setting-string"> Profile  个人</span>
+                            </div>
+                        </el-dropdown-item>
+                        <el-dropdown-item divided>
+                            <div class="setting-div">
+                                <span class="setting-icon"><i class="material-icons">settings</i></span> 
+                                <span class="setting-string"> Settings  设置</span>
+                            </div>
+                         
+                        </el-dropdown-item>
+                        <el-dropdown-item divided>
+                            <div class="setting-div">
+                                <span class="setting-icon"><i class="material-icons">assignment_return</i></span> 
+                                <span class="setting-string"> Sign out  退出</span>
+                            </div>
+                            
+                        </el-dropdown-item>
+                        
+                    </el-dropdown-menu>
+                </el-dropdown>
+
+
             </div>
+
+
          </el-col>
-
-            
-
-
-
         </el-row>
-        
+
+
+      
+
+
     </div>
+    
+
+
+
 </template>
 
 <script>
     export default {
         data() {
             return {
-                name: 'linxin'
+                name: 'linxin',
+                dialogs:[
+                    {header:'../../../static/images/b_header2.jpg',content:'我有一个Style样式需要您帮忙处理以下。',time:'3'},
+                    {header:'../../../static/images/b_header3.jpg',content:'中午一起吃个饭吧，我请客。',time:'15'},
+                    {header:'../../../static/images/b_header.jpg',content:'需要处理一下POP展示的白边样式。',time:'18'},
+                    {header:'../../../static/images/b_header2.jpg',content:'下午3点开需求会议，准时参加。',time:'24'},
+                    {header:'../../../static/images/b_header3.jpg',content:'晚上我开车送你回家，你请我吃晚饭。',time:'45'},
+                ],
+                tasks:[
+                    {id:1,rank:1,content:'完成JSPangAdmin头部头部组件的编写。',overTime:'2017/3/9'},
+                    {id:2,rank:2,content:'完成GitHub仓库的初始化工作。',overTime:'2017/3/15'},
+                    {id:3,rank:3,content:'在阿里云进行网站备案，完成后通知组长。',overTime:'2017/3/20'}
+                    
+                ]
             }
         },
         methods:{
@@ -112,27 +205,96 @@
         color:#fff;
     }
     .header-right{
-        
+        padding-top:25px;
+        line-height:10px;
     }
     .header-right span{
         display:inline-block;
         margin-left:14px;
         font-size:18px;
         color:#fff;
-        line-height:18px;
+        line-height:1px;
+        height:18px;
         cursor:pointer;
+
     }
     .header-right span:hover{
         color:#209e91;
 
     }
     .user-header{
-        line-height:50px;
+        line-height:10px;
         padding-top:7px;
+        font-size:12px;
     }
     .user-header img{
         border-radius:50%;
     }
+    .pop-image{
+        display:block;
+        float:left;
+    }
+    .pop-image img{
+        border-radius:50%;
+    }
+    .pop-div{
+        height:55px;
+        clear:both;
+    }
+    .pop-content{
+        display:block;
+        font-size:12px;
+        line-height:14px;
+        padding-top:15px;
+        margin-left:60px;
+    }
+    .pop-time{
+        display:block;
+        float:right;
+        font-size:10px;
+        color:#ccc;
+
+    }
+    .pop-title{
+        font-size:12px;
+        color:#209e91;
+    }
+    .task-div{
+        clear:both;
+        height:55px;
+    }
+    .task-span{
+        display:block;
+        float:left;
+        font-size:12px
+    }
     
+    .task-content{  
+        width:160px;
+        line-height:16px;
+        margin-left:10px;
+        padding:5px;
+    }
+    .task-time{
+        clear:both;
+        float:right;
+        color:#ccc;
+        
+    }
+    .setting-div{
+        height:30px;
+        line-height:30px;
+        clear:both;
+    }
+    .setting-div span{
+        display:block;
+        float:left;
+        font-size:12px;
+    }
+    .setting-icon{
+        padding-top:3px;
+        padding-right:3px;
+    }
+
 
 </style>
