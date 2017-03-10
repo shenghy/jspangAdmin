0be53a6fb9806773b1e6c45894ace4729690be2c
““ShengHongYu <web0432@126.com>
2017-03-10 11:39:33 +0800

    增加了DashBoard的仪表首行

---
+++ src/components/common/pageTitle.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="pageTitle">
+        <div class="pt-left"><h2>{{vtitle.toUpperCase()}}</h2></div>
+        <div class="pt-right">
+            <span class="pt-Home"><router-link to="DashBoard">Home</router-link></span>
+            <span class="pt-line"> / </span>
+            <span class="pt-page">{{vtitle}}</span>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+       props:['vtitle']
+    }
+</script>
+<style scoped>
+    .pageTitle{
+        clear:both;
+        height:55px;
+      
+    }
+    .pt-left{
+        float:left;
+    }
+    .pt-right{
+        float:right;
+    }
+    .pt-Home{
+        font-size:20px;
+    }
+    .pt-line{
+        color:#ccc;
+    }
+    a{
+        color:#209e91;
+    }
+</style>
--- src/components/page/DashBoard.vue
+++ src/components/page/DashBoard.vue
@@ -1,12 +1,133 @@
 <template>
     <div >
-        DashBoard.vue
+        <v-pageTitle vtitle="DashBoard"></v-pageTitle>
+
+        <div class="clear"></div>
+
+        <el-row :gutter="20">
+
+            <el-col :xs="12" :sm="12" :md="12" :lg="6">
+                <el-card class="box-card">
+                   <el-row :gutter="6">
+                    <el-col :xs="24" :sm="8" :md="8" :lg="8">
+                        <div> <el-progress type="circle" :percentage="67" :width="80"></el-progress></div>
+                    </el-col>
+                    <el-col :xs="24" :sm="11" :md="12" :lg="12">
+                        <div class="cart-string"> 
+                        <span>New Visits</span>
+                        <span><h2>57,820</h2></span>
+                        </div>
+                    </el-col>
+                    <el-col :xs="24" :sm="5" :md="4" :lg="4">
+                        <div>
+                            <i class="material-icons">person_outline</i>
+                        </div>
+                    </el-col>
+                   </el-row>
+                </el-card>
+            </el-col>
+
+            <el-col :xs="12" :sm="12" :md="12" :lg="6">
+                <el-card class="box-card">
+                   <el-row :gutter="6">
+                    <el-col :xs="24" :sm="8" :md="8" :lg="8">
+                        <div> <el-progress type="circle" :percentage="81" :width="80"></el-progress></div>
+                    </el-col>
+                    <el-col :xs="24" :sm="11" :md="12" :lg="12">
+                        <div class="cart-string"> 
+                        <span>Purchases</span>
+                        <span><h2>$1,680</h2></span>
+                        </div>
+                    </el-col>
+                    <el-col :xs="24" :sm="5" :md="4" :lg="4">
+                        <div>
+                            <i class="material-icons">attach_money</i>
+                        </div>
+                    </el-col>
+                   </el-row>
+                </el-card>
+            </el-col>
+
+            <el-col :xs="12" :sm="12" :md="12" :lg="6">
+                <el-card class="box-card">
+                   <el-row :gutter="6">
+                    <el-col :xs="24" :sm="8" :md="8" :lg="8">
+                        <div> <el-progress type="circle" :percentage="30" :width="80"></el-progress></div>
+                    </el-col>
+                    <el-col :xs="24" :sm="11" :md="12" :lg="12">
+                        <div class="cart-string"> 
+                        <span>Active Users</span>
+                        <span><h2>157,820</h2></span>
+                        </div>
+                    </el-col>
+                    <el-col :xs="24" :sm="5" :md="4" :lg="4">
+                        <div>
+                            <i class="material-icons">touch_app</i>
+                        </div>
+                    </el-col>
+                   </el-row>
+                </el-card>
+            </el-col>
+
+
+            <el-col :xs="12" :sm="12" :md="12" :lg="6">
+                <el-card class="box-card">
+                   <el-row :gutter="6">
+                    <el-col :xs="24" :sm="8" :md="8" :lg="8">
+                        <div> <el-progress type="circle" :percentage="88" :width="80"></el-progress></div>
+                    </el-col>
+                    <el-col :xs="24" :sm="11" :md="12" :lg="12">
+                        <div class="cart-string"> 
+                        <span>Returned</span>
+                        <span><h2>32,645</h2></span>
+                        </div>
+                    </el-col>
+                    <el-col :xs="24" :sm="5" :md="4" :lg="4">
+                        <div>
+                            <i class="material-icons">replay</i>
+                        </div>
+                    </el-col>
+                   </el-row>
+                </el-card>
+            </el-col>
+        </el-row>
+
+
     </div>
+
+
+
 </template>
 
 <script>
-    
+    import vPageTitle from '../common/pageTitle.vue';
     export default {
-        
+        data(){
+           return{
+              //percentage:10
+           }
+        },
+        components:{
+            vPageTitle
+        }
     }
 </script>
+
+<style scoped>
+    .el-col{
+        margin-bottom:16px;
+    }
+    .material-icons{
+        font-size:80px;
+        color:#ddd;
+    }
+    .box-card{
+        height:110px;
+    } 
+    .cart-string{
+        height:100px;
+        padding-top:10px;
+        font-size:1.1rem;
+        
+    }
+</style>
